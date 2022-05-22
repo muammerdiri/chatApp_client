15c5fa833b610ea82f8af54394180c2785b9c1d7
Muammer Diri <muammerdiri2@gmail.com>
2022-05-22 19:18:33 +0300

    message parameters have been adjusted.

--- src/messages/IHelloCA.java
+++ src/messages/IHelloCA.java
@@ -2,5 +2,5 @@ package messages;
 
 public interface IHelloCA  {
     byte[] successResponseMessage(byte opcode, int dataLength, byte[] data);
-    byte[] commentMessage(byte opcode, int dataLength, byte[] data);
+    byte[] commentMessage( byte[] data);
 }
