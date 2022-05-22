a5a7b91584288b24840b537a4c23f4bb8fcc4ada
Muammer Diri <muammerdiri2@gmail.com>
2022-05-22 19:18:04 +0300

    message parameters have been adjusted.

--- src/messages/HelloCA.java
+++ src/messages/HelloCA.java
@@ -14,11 +14,11 @@ public class HelloCA implements IHelloCA{
     }
 
     @Override
-    public byte[] commentMessage(byte opcode, int dataLength, byte[] data) {
+    public byte[] commentMessage(byte[] data) {
         //Main array = { header +opcode + Length + Data }
         byte [] responseData ={tools.ProtocolName.COMMAND_HEADER,tools.ProtocolName.HELLO,0x00};
 
-        short length = (short)dataLength;
+        short length = (short)data.length;
         byte[] lengthToByte = ByteBuffer.allocate(2).putShort(length).array();
         responseData =tools.Tools.byteArrayConcatenation(responseData,lengthToByte);
 
