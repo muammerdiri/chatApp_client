7055f8aaa1fa84baece23bb0c20c34170cd1c4bd
Muammer Diri <muammerdiri2@gmail.com>
2022-06-13 17:02:07 +0300

    Unnecessary parts have been deleted. Key file added.

--- src/Client.java
+++ src/Client.java
@@ -106,6 +106,6 @@ public class Client {
             e.printStackTrace();
         }
     }
-    
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 import java.io.IOException;
-import java.net.Socket;
+import java.security.Security;
 import java.util.Scanner;
+import com.sun.net.ssl.internal.ssl.Provider;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
+        Security.addProvider(new Provider());
+
+        //sunucunun sertifikasını ve genelini içeren trustStore dosyasını belirtme.
+        System.setProperty("javax.net.ssl.trustStore","myTrustStore.jts");
+
+        //trustStore dosyasının parolasını belirleme
+        System.setProperty("javax.net.ssl.trustStorePassword","123456");
+
+        //Sadece el sıkışma sürecinin ayrıntılarının dökümünü göstermek içindir (Zorunlu değil).
+        //System.setProperty("javax.net.debug","all");
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Tek kullanımlık isim giriniz:");
@@ -12,9 +26,13 @@ public class Main {
         String personUsername = scanner.nextLine();
 
 
-        Socket socket = new Socket("localhost", 1234);
+        //SSLSSocketFactory, ssl bağlamını kurar ve SSLSocket oluşturur
+        SSLSocketFactory sslsocketfactory = (SSLSocketFactory)SSLSocketFactory.getDefault();
 
-        Client client = new Client(socket, username,personUsername);
+        //SSLServerFactory önceden kurulmuş ssl bağlamını kullanarak SSLSocket oluşturun ve sunucuya bağlanın
+        SSLSocket sslSocket = (SSLSocket)sslsocketfactory.createSocket("localhost",1234);
+
+        Client client = new Client(sslSocket, username,personUsername);
         client.listenForMessage();
         client.sendMessage();
     }
